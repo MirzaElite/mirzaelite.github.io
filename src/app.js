58e8dc0f9c701ed8ae6399a4ed6395891a7f0c53
MirzaElite <126695903+MirzaElite@users.noreply.github.com>
2024-02-24 14:51:34 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,398 +1,189 @@
-const image = document.querySelector('.image');
-const hover = document.querySelector('.hover');
-const modal = document.querySelector('.modal');
-const close = document.querySelector('.close');
-
+const image = document.querySelector(".image"),
+  hover = document.querySelector(".hover"),
+  modal = document.querySelector(".modal"),
+  close = document.querySelector(".close"),
+  follow = document.querySelector(".follow"),
+  card = document.querySelector(".card"),
+  loader = document.getElementById("loader"),
+  loaderText = document.getElementById("loader-text");
+function load() {
+  const e = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  let t = null,
+    a = 0;
+  const o = loaderText.dataset.value;
+  clearInterval(t),
+    (t = setInterval(() => {
+      (loaderText.innerText = o
+        .split("")
+        .map((t, n) => (n < a ? o[n] : e[Math.floor(62 * Math.random())]))
+        .join("")),
+        a >= o.length && clearInterval(t),
+        (a += 3 / o.length);
+    }, 30)),
+    setTimeout(unload, 3e3);
+}
+function unload() {
+  (loader.style.zIndex = 0), (loader.style.opacity = 0);
+}
 function show() {
-     hover.classList.add('active');
-     modal.classList.add('show');
+  hover.classList.add("active"), modal.classList.add("show");
 }
-
 function hide() {
-     hover.classList.remove('active');
-     modal.classList.remove('show');
+  hover.classList.remove("active"), modal.classList.remove("show");
 }
 
-image.addEventListener('click', show);
-close.addEventListener('click', hide);
+// loaderText.ontouchend = e => { document.documentElement.requestFullscreen(), setTimeout(load, 500) }, loaderText.onclick = e => { document.documentElement.requestFullscreen(), setTimeout(load, 500) },
 
+image.addEventListener("click", show), close.addEventListener("click", hide);
 var chkbtn = document.getElementById("chkbtn");
-const sidebar = document.getElementById('sidebar');
-
+const sidebar = document.getElementById("sidebar");
 function toogle() {
-     var chk = document.getElementById('check').checked;
-     if (!chk) {
-          sidebar.style.display = 'block';
-     } else {
-          sidebar.style.display = 'none';
-     }
+  document.getElementById("check").checked
+    ? (sidebar.style.animation = "fadeOut 0.25s linear forwards")
+    : ((sidebar.style.display = "block"),
+      (sidebar.style.animation = "fadeIn 0.25s linear"));
 }
-
-chkbtn.addEventListener('click', toogle);
-
-const container = document.querySelector('#body');
-const toggle = document.querySelector('.toggle-input');
-const part = document.getElementById('particles-js');
-const initialState = 'false';
-toggle.checked = initialState;
-
-toggle.addEventListener('change', function () {
-     if (!toggle.checked) {
-          container.className = 'light';
-          part.style.backgroundColor = "#d3d3d3";
-          particlesJS("particles-js", {
-               "particles": {
-                    "number": {
-                         "value": 100,
-                         "density": {
-                              "enable": true,
-                              "value_area": 800
-                         }
-                    },
-                    "color": {
-                         "value": "#000000"
-                    },
-                    "shape": {
-                         "type": "circle",
-                         "stroke": {
-                              "width": 0,
-                              "color": "#000000"
-                         },
-                         "polygon": {
-                              "nb_sides": 5
-                         },
-                         "image": {
-                              "src": "img/github.svg",
-                              "width": 100,
-                              "height": 100
-                         }
-                    },
-                    "opacity": {
-                         "value": 0.5,
-                         "random": false,
-                         "anim": {
-                              "enable": false,
-                              "speed": 1,
-                              "opacity_min": 0.1,
-                              "sync": false
-                         }
-                    },
-                    "size": {
-                         "value": 3,
-                         "random": true,
-                         "anim": {
-                              "enable": false,
-                              "speed": 40,
-                              "size_min": 0.1,
-                              "sync": false
-                         }
-                    },
-                    "line_linked": {
-                         "enable": true,
-                         "distance": 150,
-                         "color": "#000000",
-                         "opacity": 0.4,
-                         "width": 1
-                    },
-                    "move": {
-                         "enable": true,
-                         "speed": 6,
-                         "direction": "none",
-                         "random": false,
-                         "straight": false,
-                         "out_mode": "out",
-                         "bounce": false,
-                         "attract": {
-                              "enable": false,
-                              "rotateX": 600,
-                              "rotateY": 1200
-                         }
-                    }
-               },
-               "interactivity": {
-                    "detect_on": "canvas",
-                    "events": {
-                         "onhover": {
-                              "enable": true,
-                              "mode": "repulse"
-                         },
-                         "onclick": {
-                              "enable": true,
-                              "mode": "push"
-                         },
-                         "resize": true
-                    },
-                    "modes": {
-                         "grab": {
-                              "distance": 400,
-                              "line_linked": {
-                                   "opacity": 1
-                              }
-                         },
-                         "bubble": {
-                              "distance": 400,
-                              "size": 40,
-                              "duration": 2,
-                              "opacity": 8,
-                              "speed": 3
-                         },
-                         "repulse": {
-                              "distance": 200,
-                              "duration": 0.4
-                         },
-                         "push": {
-                              "particles_nb": 4
-                         },
-                         "remove": {
-                              "particles_nb": 2
-                         }
-                    }
-               },
-               "retina_detect": true
-          })
-          kofiWidgetOverlay.draw('GreatMirza', {
-               'type': 'floating-chat',
-               'floating-chat.donateButton.text': 'Support me',
-               'floating-chat.donateButton.background-color': '#00b9fe',
-               'floating-chat.donateButton.text-color': '#fff'
-          });
-     }
-     else {
-          container.className = 'dark';
-          part.style.backgroundColor = "#161616";
-          particlesJS("particles-js", {
-               "particles": {
-                    "number": {
-                         "value": 100,
-                         "density": {
-                              "enable": true,
-                              "value_area": 800
-                         }
-                    },
-                    "color": {
-                         "value": "#f70000"
-                    },
-                    "shape": {
-                         "type": "circle",
-                         "stroke": {
-                              "width": 0,
-                              "color": "#000000"
-                         },
-                         "polygon": {
-                              "nb_sides": 5
-                         },
-                         "image": {
-                              "src": "img/github.svg",
-                              "width": 100,
-                              "height": 100
-                         }
-                    },
-                    "opacity": {
-                         "value": 0.5,
-                         "random": false,
-                         "anim": {
-                              "enable": false,
-                              "speed": 1,
-                              "opacity_min": 0.1,
-                              "sync": false
-                         }
-                    },
-                    "size": {
-                         "value": 3,
-                         "random": true,
-                         "anim": {
-                              "enable": false,
-                              "speed": 40,
-                              "size_min": 0.1,
-                              "sync": false
-                         }
-                    },
-                    "line_linked": {
-                         "enable": true,
-                         "distance": 150,
-                         "color": "#ffffff",
-                         "opacity": 0.4,
-                         "width": 1
-                    },
-                    "move": {
-                         "enable": true,
-                         "speed": 6,
-                         "direction": "none",
-                         "random": false,
-                         "straight": false,
-                         "out_mode": "out",
-                         "bounce": false,
-                         "attract": {
-                              "enable": false,
-                              "rotateX": 600,
-                              "rotateY": 1200
-                         }
-                    }
-               },
-               "interactivity": {
-                    "detect_on": "canvas",
-                    "events": {
-                         "onhover": {
-                              "enable": true,
-                              "mode": "repulse"
-                         },
-                         "onclick": {
-                              "enable": true,
-                              "mode": "push"
-                         },
-                         "resize": true
-                    },
-                    "modes": {
-                         "grab": {
-                              "distance": 400,
-                              "line_linked": {
-                                   "opacity": 1
-                              }
-                         },
-                         "bubble": {
-                              "distance": 400,
-                              "size": 40,
-                              "duration": 2,
-                              "opacity": 8,
-                              "speed": 3
-                         },
-                         "repulse": {
-                              "distance": 200,
-                              "duration": 0.4
-                         },
-                         "push": {
-                              "particles_nb": 4
-                         },
-                         "remove": {
-                              "particles_nb": 2
-                         }
-                    }
-               },
-               "retina_detect": true
-          })
-          kofiWidgetOverlay.draw('GreatMirza', {
-               'type': 'floating-chat',
-               'floating-chat.donateButton.text': 'Support me',
-               'floating-chat.donateButton.background-color': '#323842',
-               'floating-chat.donateButton.text-color': '#fff'
-          });
-     }
-});
-
-particlesJS("particles-js", {
-     "particles": {
-          "number": {
-               "value": 100,
-               "density": {
-                    "enable": true,
-                    "value_area": 800
-               }
+chkbtn.addEventListener("click", toogle);
+const container = document.querySelector("#body"),
+  toggle = document.querySelector(".toggle-input"),
+  part = document.getElementById("particles-js"),
+  initialState = "false";
+(toggle.checked = "false"),
+  toggle.addEventListener("change", function () {
+    toggle.checked
+      ? ((container.className = "dark"),
+        (part.style.backgroundColor = "#100b16"),
+        card.style.setProperty(
+          "--anim",
+          "cardDarkGradient .3s ease-in forwards"
+        ),
+        (follow.style.animation =
+          "followDarkGradient .3s ease-in-out forwards"),
+        particlesJS("particles-js", {
+          particles: {
+            number: { value: 100, density: { enable: !0, value_area: 800 } },
+            color: { value: "#b392ac" },
+            shape: { type: "polygon", polygon: { nb_sides: 6 } },
+            opacity: { value: 0.75 },
+            size: {
+              value: 2,
+              anim: { enable: !0, speed: 5, size_min: 1, sync: !0 }
+            },
+            line_linked: {
+              enable: !0,
+              distance: 125,
+              color: "#ffffff",
+              opacity: 0.75,
+              width: 0.5
+            },
+            move: {
+              enable: !0,
+              speed: 5,
+              attract: { enable: !0, rotateX: 1500, rotateY: 900 }
+            }
           },
-          "color": {
-               "value": "#f70000"
+          interactivity: {
+            detect_on: "canvas",
+            events: {
+              onhover: { enable: !0, mode: "grab" },
+              onclick: { enable: !0, mode: "bubble" },
+              resize: !0
+            },
+            modes: {
+              grab: { distance: 300, line_linked: { opacity: 1 } },
+              bubble: {
+                distance: 300,
+                size: 5,
+                duration: 0.75,
+                opacity: 8,
+                speed: 3
+              }
+            }
           },
-          "shape": {
-               "type": "circle",
-               "stroke": {
-                    "width": 0,
-                    "color": "#000000"
-               },
-               "polygon": {
-                    "nb_sides": 5
-               },
-               "image": {
-                    "src": "img/github.svg",
-                    "width": 100,
-                    "height": 100
-               }
+          retina_detect: !0
+        }))
+      : ((container.className = "light"),
+        (part.style.backgroundColor = "#eed1ff"),
+        card.style.setProperty(
+          "--anim",
+          "cardLightGradient .3s ease-in-out forwards"
+        ),
+        (follow.style.animation =
+          "followLightGradient .3s ease-in-out forwards"),
+        particlesJS("particles-js", {
+          particles: {
+            number: { value: 100, density: { enable: !0, value_area: 800 } },
+            color: { value: "#7B2CBF" },
+            shape: { type: "circle" },
+            opacity: { value: 0.75 },
+            size: {
+              value: 2,
+              anim: { enable: !0, speed: 5, size_min: 1, sync: !0 }
+            },
+            line_linked: {
+              enable: !0,
+              distance: 125,
+              color: "#7B2CBF",
+              opacity: 2,
+              width: 0.5
+            },
+            move: {
+              enable: !0,
+              speed: 5,
+              attract: { enable: !0, rotateX: 1500, rotateY: 900 }
+            }
           },
-          "opacity": {
-               "value": 0.5,
-               "random": false,
-               "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-               }
+          interactivity: {
+            detect_on: "canvas",
+            events: {
+              onhover: { enable: !0, mode: "repulse" },
+              onclick: { enable: !0, mode: "bubble" },
+              resize: !0
+            },
+            modes: {
+              bubble: {
+                distance: 300,
+                size: 5,
+                duration: 0.75,
+                opacity: 8,
+                speed: 3
+              }
+            }
           },
-          "size": {
-               "value": 3,
-               "random": true,
-               "anim": {
-                    "enable": false,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": false
-               }
-          },
-          "line_linked": {
-               "enable": true,
-               "distance": 150,
-               "color": "#ffffff",
-               "opacity": 0.4,
-               "width": 1
-          },
-          "move": {
-               "enable": true,
-               "speed": 6,
-               "direction": "none",
-               "random": false,
-               "straight": false,
-               "out_mode": "out",
-               "bounce": false,
-               "attract": {
-                    "enable": false,
-                    "rotateX": 600,
-                    "rotateY": 1200
-               }
-          }
-     },
-     "interactivity": {
-          "detect_on": "canvas",
-          "events": {
-               "onhover": {
-                    "enable": true,
-                    "mode": "repulse"
-               },
-               "onclick": {
-                    "enable": true,
-                    "mode": "push"
-               },
-               "resize": true
-          },
-          "modes": {
-               "grab": {
-                    "distance": 400,
-                    "line_linked": {
-                         "opacity": 1
-                    }
-               },
-               "bubble": {
-                    "distance": 400,
-                    "size": 40,
-                    "duration": 2,
-                    "opacity": 8,
-                    "speed": 3
-               },
-               "repulse": {
-                    "distance": 200,
-                    "duration": 0.4
-               },
-               "push": {
-                    "particles_nb": 4
-               },
-               "remove": {
-                    "particles_nb": 2
-               }
-          }
-     },
-     "retina_detect": true
-})
-
-kofiWidgetOverlay.draw('GreatMirza', {
-               'type': 'floating-chat',
-               'floating-chat.donateButton.text': 'Support me',
-               'floating-chat.donateButton.background-color': '#323842',
-               'floating-chat.donateButton.text-color': '#fff'
-          });
+          retina_detect: !0
+        }));
+  }),
+  particlesJS("particles-js", {
+    particles: {
+      number: { value: 80, density: { enable: !0, value_area: 800 } },
+      color: { value: "#b392ac" },
+      shape: { type: "polygon", polygon: { nb_sides: 6 } },
+      opacity: { value: 0.75 },
+      size: { value: 2, anim: { enable: !0, speed: 5, size_min: 1, sync: !0 } },
+      line_linked: {
+        enable: !0,
+        distance: 125,
+        color: "#ffffff",
+        opacity: 0.75,
+        width: 0.5
+      },
+      move: {
+        enable: !0,
+        speed: 5,
+        attract: { enable: !0, rotateX: 1500, rotateY: 900 }
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: { enable: !0, mode: "grab" },
+        onclick: { enable: !0, mode: "bubble" },
+        resize: !0
+      },
+      modes: {
+        grab: { distance: 300, line_linked: { opacity: 1 } },
+        bubble: { distance: 300, size: 5, duration: 0.75, opacity: 8, speed: 3 }
+      }
+    },
+    retina_detect: !0
+  });
